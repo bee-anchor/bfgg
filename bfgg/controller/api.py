42fe954d688608d023e2c9e13ec4fe15fbcd305a
David-IW <48682759+David-IW@users.noreply.github.com>
2019-11-04 17:50:06 +0000

    S6 2175 (#4)
    * Added marshmallow validation for clone and start endpoints
    
    * Added newline to end of api_schemas file

--- bfgg/controller/api.py
+++ bfgg/controller/api.py
@@ -6,6 +6,8 @@ from bfgg.controller.state import Task
 from bfgg.utils.messages import CLONE, PREP_TEST, START_TEST, STOP_TEST, STATUS
 from bfgg.controller.model import LOCK, STATE, CONTEXT
 from bfgg.controller.results_getter import ResultsGetter
+from bfgg.controller.api_schemas import StartSchema, CloneSchema
+from marshmallow import ValidationError, EXCLUDE
 
 bp = Blueprint('root', __name__)
 
@@ -16,10 +18,15 @@ gatling_location = os.getenv('GATLING_LOCATION')
 s3_bucket = os.getenv('S3_BUCKET')
 s3_region = os.getenv('S3_REGION')
 
+bad_request = 400
+
 @bp.route('/clone', methods=['POST'])
 def clone():
-    body = request.get_json(force=True)
-    repo = body['repo']
+    try:
+        result = CloneSchema().load(request.get_json(force=True), unknown=EXCLUDE)
+    except ValidationError as err:
+       return err.messages, bad_request
+    repo = result['repo']
     with LOCK:
         STATE.add_task(Task(CLONE, b'MASTER', repo.encode('utf-8')))
     return {
@@ -29,10 +36,13 @@ def clone():
 
 @bp.route('/start', methods=['POST'])
 def start():
-    body: dict = request.get_json(force=True)
-    project = body['project']
-    test = body['testClass']
-    javaOpts = body.get('javaOpts', '')
+    try:
+        result = StartSchema().load(request.get_json(force=True), unknown=EXCLUDE)
+    except ValidationError as err:
+       return err.messages, bad_request
+    project = result['project']
+    test = result['testClass']
+    javaOpts = result.get('javaOpts', '')
     task = f"{project},{test},{javaOpts}".encode('utf-8')
     with LOCK:
         STATE.add_task(Task(START_TEST, b'MASTER', task))
