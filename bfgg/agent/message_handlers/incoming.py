441c9a42006295f76ca44007bc684eccf6157975
David Jones <david.jones@infinityworks.com>
2019-11-26 09:51:36 +0000

    Added tests for agent incoming and outgoing message handlers

--- bfgg/agent/message_handlers/incoming.py
+++ bfgg/agent/message_handlers/incoming.py
@@ -18,27 +18,29 @@ class IncomingMessageHandler(threading.Thread):
         self.results_folder = results_folder
         self.gatling_location = gatling_location
         self.test_runner = None
+        self.handler = self.context.socket(zmq.PULL)
+        self.handler.connect(f"tcp://{self.controller_host}:{self.port}")
 
     def run(self):
-        handler = self.context.socket(zmq.PULL)
-        handler.connect(f"tcp://{self.controller_host}:{self.port}")
         logging.info("IncomingMessageHandler thread started")
         while True:
             try:
-                [identity, type, message] = handler.recv_multipart()
-                if type == CLONE:
-                    clone_repo(message.decode("utf-8"), self.tests_location)
-                elif type == START_TEST:
-                    project, test, java_opts = message.decode('utf-8').split(",")
-                    self.test_runner = TestRunner(
-                        self.gatling_location, self.tests_location, self.results_folder, project, test, java_opts
-                    )
-                    self.test_runner.start()
-                elif type == STOP_TEST:
-                    if self.test_runner:
-                        self.test_runner.stop_runner = True
-                else:
-                    print(identity, type, message)
+                self._message_handler_loop()
             except Exception as e:
                 logging.error(e)
                 continue
+
+    def _message_handler_loop(self):
+        [identity, type, message] = self.handler.recv_multipart()
+        if type == CLONE:
+            clone_repo(message.decode("utf-8"), self.tests_location)
+        elif type == START_TEST:
+            project, test, java_opts = message.decode('utf-8').split(",")
+            self.test_runner = TestRunner(
+                self.gatling_location, self.tests_location, self.results_folder, project, test, java_opts
+            )
+            self.test_runner.start()
+        elif type == STOP_TEST and self.test_runner is not None:
+            self.test_runner.stop_runner = True
+        else:
+            logging.warning(f'Received unhandled message, it has been dropped: {identity}, {type}, {message}')
